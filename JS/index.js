0b8b78d16e2aa08d87e07524bf7bac5a89a2e1c6
SeshuYaswanth <seshuyaswanthreddy01@gmail.com>
2025-03-02 01:12:18 +0530

    final

--- JS/index.js
+++ JS/index.js
@@ -22,17 +22,17 @@ async function search(city) {
     document.querySelector(".error").style.display = "none";
 
     if (data.weather[0].main === "Clouds") {
-      weatherIcon.src = "../images/clouds.png";
+      weatherIcon.src = "/images/clouds.png";
     } else if (data.weather[0].main === "Clear") {
-      weatherIcon.src = "../images/clear.png";
+      weatherIcon.src = "/images/clear.png";
     } else if (data.weather[0].main === "Drizzle") {
-      weatherIcon.src = "../images/drizzle.png";
+      weatherIcon.src = "/images/drizzle.png";
     } else if (data.weather[0].main === "Rain") {
-      weatherIcon.src = "../images/rain.png";
+      weatherIcon.src = "/images/rain.png";
     } else if (data.weather[0].main === "Snow") {
-      weatherIcon.src = "../images/snow.png";
+      weatherIcon.src = "/images/snow.png";
     } else {
-      weatherIcon.src = "../images/mist.png";
+      weatherIcon.src = "/images/mist.png";
     }
   }
 }
